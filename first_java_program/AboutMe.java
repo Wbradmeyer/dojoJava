356b08c2fc27378b6ec1bbaac5b1afb19067451c
Wbradmeyer <107085402+Wbradmeyer@users.noreply.github.com>
2023-09-07 19:38:33 -0400

    Add files via upload

---
+++ first_java_program/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Brad Meyer.");
+        System.out.println("I am 40 years old.");
+        System.out.println("My hometown is Johns Creek, GA.");
+    }
+}
