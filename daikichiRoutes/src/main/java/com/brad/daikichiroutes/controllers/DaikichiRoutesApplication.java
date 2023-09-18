b730e51fa028b029ab621212ee63df28bf8b7e44
Wbradmeyer <107085402+Wbradmeyer@users.noreply.github.com>
2023-09-18 14:52:44 -0400

    Add files via upload

---
+++ daikichiRoutes/src/main/java/com/brad/daikichiroutes/controllers/DaikichiRoutesApplication.java
@@ -0,0 +1,12 @@
+package com.brad.daikichiroutes.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+}
